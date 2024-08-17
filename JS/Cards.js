0389cd2a1132b614204a94ebbd3f8c20c9b3c238
Anti27 <anti27@gmx.ch>
2024-08-17 14:06:03 +0200

    Update Cards.js

--- JS/Cards.js
+++ JS/Cards.js
@@ -2,12 +2,14 @@ let cardStuff = getCardData();
 
 function predictCards() {
     const div = document.getElementById("showCardsHere");
+    let amount = document.getElementById("cardAmount").value;
+    if (amount < 1 && amount > 1000000) {amount = 10}
     var dict = JSON.parse(document.getElementById("saveData").innerHTML);
     while(div.firstChild) { div.removeChild(div.firstChild); }
     let selectedPack = document.getElementById("selectedPack").value;
     let pack = cardStuff.packs[selectedPack]
     var prog = typeof parseInt(document.getElementById('cardRng').value) === "number" ? document.getElementById('cardRng').value : 0;
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < amount; i++) {
         outputText("------  " + (i + 1) + "  ------")
         let rngGen = new Math.seedrandom(document.getElementById('playerID').value + "cardPack_" + String(selectedPack) + "_" + (parseInt(prog) + i));
         let cacheWeight = [];
