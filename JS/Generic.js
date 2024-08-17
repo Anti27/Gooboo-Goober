68d17167e8b0e09828cc540a2c61567e713a1300
Anti27 <anti27@gmx.ch>
2024-08-17 12:43:18 +0200

    Update Generic.js

--- JS/Generic.js
+++ JS/Generic.js
@@ -95,3 +95,19 @@ function weightSelect(weights, rng = Math.random()) {
         return false;
     })
 }
+
+function openElement(evt, featureName) {
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(featureName).style.display = "block";
+  evt.currentTarget.className += " active";
+} 
