239fed0e994ae4be3afb3b4ae79a24e3603a7f9c
Anti27 <anti27@gmx.ch>
2024-09-15 21:33:06 +0200

    Update Bingo.js

--- JS/Bingo.js
+++ JS/Bingo.js
@@ -209,6 +209,7 @@ function solver(showResult, currentWeights, internalCard, internalDraw, internal
             showResult[4] = bingoCount;
             showResult[5] = structuredClone(currentWeights);
             showResult[6] = structuredClone(newInternalDraw);
+            alert("Combinations: " + showResult[2] + " Count: " + showResult[4] + " Weights: " + showResult[5] + " Draw: " + showResult[6]);
             console.log("Combinations: " + showResult[2] + " Count: " + showResult[4] + " Weights: " + showResult[5] + " Draw: " + showResult[6]);
             document.getElementById("bingoCombinations").innerText = "Combinations: " + showResult[2];
             document.getElementById("bingoCount").innerText = "Count: " + showResult[4];
